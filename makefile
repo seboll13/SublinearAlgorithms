356ffa5b64ee89c712a3546a937cf49407f2d3d8
Sebastien Ollquist <seboll@Sebastiens-Mac-mini-2.local>
2022-05-20 12:47:37 +0200

    test push from makefile

---
+++ makefile
@@ -0,0 +1,25 @@
+CPATH=./C/
+TESTPATH=./test/
+M="default push from makefile message"
+
+all: run tests
+
+run:
+	$(MAKE) -C $(CPATH)
+
+tests:
+	$(MAKE) -C $(TESTPATH)
+
+run_tests: tests
+	$(TESTPATH)vector_test
+
+push:
+	git add .
+	git commit -m "$(M)"
+	git push
+
+.PHONY: all clean
+
+clean:
+	$(MAKE) clean -C $(CPATH)
+	$(MAKE) clean -C $(TESTPATH)
--- test/vector_test.c
+++ test/vector_test.c
@@ -9,6 +9,21 @@ START_TEST(test_empty_vector_creation)
         ck_assert_float_eq(creal(v->items[i]), 0.0f);
         ck_assert_float_eq(cimag(v->items[i]), 0.0f);
     }
+    free_vector(v);
+}
+END_TEST
+
+START_TEST(test_update_vector)
+{
+    Vector *v = malloc(sizeof(Vector));
+    init_vector(v, "V", 3);
+    for (int i = 0; i < 3; i++)
+        update_vector(v, 1.0f + 1.0f*I, i);
+    for (int i = 0; i < 3; i++) {
+        ck_assert_float_eq(creal(v->items[i]), 1.0f);
+        ck_assert_float_eq(cimag(v->items[i]), 1.0f);
+    }
+    free_vector(v);
 }
 END_TEST
 
@@ -21,6 +36,7 @@ Suite *vector_suite(void)
 
     tc_core = tcase_create("Core");
     tcase_add_test(tc_core, test_empty_vector_creation);
+    tcase_add_test(tc_core, test_update_vector);
     suite_add_tcase(s, tc_core);
 
     return s;
